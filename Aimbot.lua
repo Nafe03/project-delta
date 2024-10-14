409e6c3a559966e2a221ecbfac6605d2fe258833
Nafe03 <rustpro29@gmail.com>
2024-10-14 19:06:25 +0200

    Update Aimbot.lua

--- Aimbot.lua
+++ Aimbot.lua
@@ -6,7 +6,7 @@ local TweenService = game:GetService("TweenService")
 local LocalPlayer = Players.LocalPlayer
 local Holding = false
 
-_G.AimbotEnabled = true
+_G.AimbotEnabled = false
 _G.TeamCheck = false -- If set to true then the script would only lock your aim at enemy team members.
 _G.AimPart = nil -- Where the aimbot script would lock at.
 _G.Sensitivity = 0 -- How many seconds it takes for the aimbot script to officially lock onto the target's aimpart.
