c8050ab80fdce00d2f00d5b8780b4fceefab7d6d
Nafe03 <rustpro29@gmail.com>
2024-11-12 22:02:24 +0100

    Update EspEXE.lua

--- EspEXE.lua
+++ EspEXE.lua
@@ -131,6 +131,10 @@ local function updateESP(player)
 
         if _G.HealthESPEnabled then
             local healthFraction = humanoid.Health / humanoid.MaxHealth
+            if healthFraction > 50 then
+                _G.HighlightColor = Color3.fromRGB(255, 255, 255)
+            else
+                _G.HighlightColor = Color3.fromRGB(0, 255, 0)
             highlight.FillTransparency = 1 - healthFraction
         else
             highlight.FillTransparency = 1
