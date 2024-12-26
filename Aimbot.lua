8876213b45e748936af0baa809ad6e86a3eec6f0
Nafe03 <rustpro29@gmail.com>
2024-12-26 17:07:43 +0100

    Update Aimbot.lua

--- Aimbot.lua
+++ Aimbot.lua
@@ -17,7 +17,7 @@ _G.TeamCheck = false
 _G.AimPart = "Head"
 _G.AirAimPart = "LowerTorso"
 _G.Sensitivity = 0       -- Smoothness level (lower = faster)
-_G.PredictionAmount = 0.25       -- Horizontal prediction for moving targets
+_G.PredictionAmount = 0       -- Horizontal prediction for moving targets
 _G.AirPredictionAmount = 0    -- Vertical prediction for airborne targets
 _G.BulletDropCompensation = 0
 _G.DistanceAdjustment = false
