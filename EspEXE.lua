afa44435038726f0a8821069f373150f7596b09a
Nafe03 <rustpro29@gmail.com>
2024-12-30 21:14:32 +0100

    Update EspEXE.lua

--- EspEXE.lua
+++ EspEXE.lua
@@ -131,9 +131,9 @@ local function DrawESPBox(player)
             local screenPos, onScreen = Camera:WorldToViewportPoint(rootPos)
 
             if onScreen then
-                local size = Vector2.new(4000 / screenPos.Z, 5000 / screenPos.Z)
+                local size = Vector2.new(3700 / screenPos.Z, 4700 / screenPos.Z)
                 box.Size = size
-                box.Position = Vector2.new(screenPos.X - size.X / 1, screenPos.Y - size.Y / 1)
+                box.Position = Vector2.new(screenPos.X - size.X / 2, screenPos.Y - size.Y / 2)
                 box.Color = _G.BoxColor
                 box.Visible = true
             else
