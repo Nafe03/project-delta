f458bd3b03d742a8a0221486c59ea37b12cb506f
Nafe03 <rustpro29@gmail.com>
2025-01-05 09:56:22 +0100

    Update Aimbot.lua

--- Aimbot.lua
+++ Aimbot.lua
@@ -173,7 +173,7 @@ local function PredictTargetPosition(Target)
     local movementDelta = (HumanoidRootPart.Position - lastPosition).Magnitude
     character:SetAttribute("LastPosition", HumanoidRootPart.Position)
 
-    local timeDelta = math.clamp(workspace.DistributedGameTime - (character:GetAttribute("LastUpdateTime") or 0), 0.02, 0.15)
+    local timeDelta = math.clamp(workspace.DistributedGameTime - (character:GetAttribute("LastUpdateTime") or 0.02), 0.017, 0.12)
     character:SetAttribute("LastUpdateTime", workspace.DistributedGameTime)
 
     local maxAllowedDelta = Speed * timeDelta + 20 -- Threshold based on speed and time delta
