ad32f6a6c17870161aa87ef9eacba31c115ced25
Nafe03 <rustpro29@gmail.com>
2025-07-05 01:11:24 +0200

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Nafe03/project-delta/refs/heads/main/ape34.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Nafe03/project-delta/refs/heads/main/ape3.lua"))()
