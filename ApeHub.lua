de0df2355a05ceaf8fe27f563fe03aeb9b31046d
Nafe03 <rustpro29@gmail.com>
2025-07-17 20:27:28 +0200

    Update ApeHub.lua

--- ApeHub.lua
+++ ApeHub.lua
@@ -1437,4 +1437,41 @@ function UILibrary.new(options)
     return Window
 end
 
+local dragToggle = nil
+local dragSpeed = 0.25
+local dragStart = nil
+local startPos = nil
+
+local function updateInput(input)
+    local delta = input.Position - dragStart
+    local position = UDim2.new(
+        startPos.X.Scale, 
+        startPos.X.Offset + delta.X, 
+        startPos.Y.Scale, 
+        startPos.Y.Offset + delta.Y
+    )
+    TweenService:Create(MainBackGround, TweenInfo.new(dragSpeed), {Position = position}):Play()
+end
+
+MainBackGround.InputBegan:Connect(function(input)
+    if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then
+        dragToggle = true
+        dragStart = input.Position
+        startPos = MainBackGround.Position
+        input.Changed:Connect(function()
+            if input.UserInputState == Enum.UserInputState.End then
+                dragToggle = false
+            end
+        end)
+    end
+end)
+
+UserInputService.InputChanged:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+        if dragToggle then
+            updateInput(input)
+        end
+    end
+end)
+
 return UILibrary
